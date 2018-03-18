0bcc163fa6445a38b6ca0b7735d4697aa46201d9
Omar <omar.diouri.pc@gmail.com>
2018-03-18 18:03:02 +0000

    another bunch of changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,9 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested'),
-    cssImport = require('postcss-import');
-
-gulp.task('default', function(){
-    console.log("Horray - you created a Gulp task.");
-});
-
-gulp.task('html', function(){
-    console.log("Imagine something useful being done to your HTML here");
-});
-
-gulp.task('styles', function(){
-    console.log("Imagine something useful being done to your CSS here");
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch',function(){
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch')
     
-    watch('./app/index.html', function(){
-       gulp.start('html');
-    });
-    
-    watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
-    });
     
 
-    
-});
+
+
+
+
